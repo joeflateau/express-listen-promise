d4f136f7eaf059e1961004c754fc0015e1022078
Joe Flateau <joe@joeflateau.net>
2022-02-01 14:42:51 -0500

    don't use labeled tuple as it breaks compat with older typescript apps

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,15 @@
 import { Express } from 'express';
 import { Server } from 'http';
 
-export type ListenOptions = [port: number, hostname: string] | [port: number];
-
 export async function expressListenPromise(
   app: Express,
-  ...options: ListenOptions
+  port: number,
+  hostname?: string
 ) {
+  const listenOptions = [port, ...(hostname != null ? [hostname] : [])];
   return new Promise<Server>((resolve, reject) => {
     const server = app
-      .listen(...(options as any[]), () => {
+      .listen(...(listenOptions as any[]), () => {
         resolve(server);
       })
       .on('error', (err) => reject(err));
